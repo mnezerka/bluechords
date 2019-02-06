2f7cefee0771f08b4ea339f5443b3a0c163f7ba8
Michal Nezerka <michal.nezerka@sap.com>
2019-02-06 08:20:41 +0100

    Fix of filesystem attributes for some files

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,5 +1,6 @@
 async function songs(root, args, context)
 {
+    // this doesn't work, no idea what could be wrong
     //const where = args.filter ? { OR: [{ name_contains : args.filter }, { content_contains: args.filter }] } : {}
 
     const where = args.filter ? {name_contains : args.filter} : {}
