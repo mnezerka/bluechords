254181b37e27b114aaec4befc0d27ac883945fac
michaln <michal.nezerka@gmail.com>
2019-02-27 23:32:11 +0100

    Switch db engine mongodb -> mysql

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -20,9 +20,9 @@ async function user(root, args, context)
 async function songs(root, args, context)
 {
     // this doesn't work, no idea what could be wrong
-    //const where = args.filter ? { OR: [{ name_contains : args.filter }, { content_contains: args.filter }] } : {}
+    const where = args.filter ? { OR: [{ name_contains : args.filter }, { artist_contains: args.filter }] } : {}
 
-    const where = args.filter ? {name_contains : args.filter} : {}
+    //const where = args.filter ? {name_contains : args.filter} : {}
 
     return await context.prisma.songs({
         where,
