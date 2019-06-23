b5e6635aeb62f3804aaff07b27869f27932fe6b0
michaln <michal.nezerka@gmail.com>
2019-06-23 23:38:59 +0200

    Server starter imported

---
+++ server_go/server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    gcontext "context"
+    h "handler"
+    "resolver"
+    "schema"
+    "service"
+    "log"
+    "net/http"
+
+    "loader"
+    graphql "github.com/graph-gophers/graphql-go"
+    "golang.org/x/net/context"
+)
+
+func main() {
+    config := gcontext.LoadConfig(".")
+
+    db, err := gcontext.OpenDB(config)
+    if err != nil {
+        log.Fatalf("Unable to connect to db: %s \n", err)
+    }
+    ctx := context.Background()
+    log := service.NewLogger(config)
+    roleService := service.NewRoleService(db, log)
+    userService := service.NewUserService(db, roleService, log)
+    authService := service.NewAuthService(config, log)
+
+    ctx = context.WithValue(ctx, "config", config)
+    ctx = context.WithValue(ctx, "log", log)
+    ctx = context.WithValue(ctx, "roleService", roleService)
+    ctx = context.WithValue(ctx, "userService", userService)
+    ctx = context.WithValue(ctx, "authService", authService)
+
+    graphqlSchema := graphql.MustParseSchema(schema.GetRootSchema(), &resolver.Resolver{})
+
+    http.Handle("/login", h.AddContext(ctx, h.Login()))
+
+    loggerHandler := &h.LoggerHandler{config.DebugMode}
+    http.Handle("/query", h.AddContext(ctx, loggerHandler.Logging(h.Authenticate(&h.GraphQL{Schema: graphqlSchema, Loaders: loader.NewLoaderCollection()}))))
+
+    http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "graphiql.html")
+    }))
+
+    log.Fatal(http.ListenAndServe(":3000", nil))
+}
---
+++ server_go/service/user_service.go
@@ -0,0 +1,108 @@
+package service
+
+import (
+	"database/sql"
+	"errors"
+	"github.com/OscarYuen/go-graphql-starter/context"
+	"github.com/OscarYuen/go-graphql-starter/model"
+	"github.com/jmoiron/sqlx"
+	"github.com/op/go-logging"
+	"github.com/rs/xid"
+)
+
+const (
+	defaultListFetchSize = 10
+)
+
+type UserService struct {
+	db          *sqlx.DB
+	roleService *RoleService
+	log         *logging.Logger
+}
+
+func NewUserService(db *sqlx.DB, roleService *RoleService, log *logging.Logger) *UserService {
+	return &UserService{db: db, roleService: roleService, log: log}
+}
+
+func (u *UserService) FindByEmail(email string) (*model.User, error) {
+	user := &model.User{}
+
+	userSQL := `SELECT * FROM users WHERE email = $1`
+	udb := u.db.Unsafe()
+	row := udb.QueryRowx(userSQL, email)
+	err := row.StructScan(user)
+	if err == sql.ErrNoRows {
+		return user, nil
+	}
+	if err != nil {
+		u.log.Errorf("Error in retrieving user : %v", err)
+		return nil, err
+	}
+
+	roles, err := u.roleService.FindByUserId(&user.ID)
+	if err != nil {
+		u.log.Errorf("Error in retrieving roles : %v", err)
+		return nil, err
+	}
+	user.Roles = roles
+	return user, nil
+}
+
+func (u *UserService) CreateUser(user *model.User) (*model.User, error) {
+	userId := xid.New()
+	user.ID = userId.String()
+	userSQL := `INSERT INTO users (id, email, password, ip_address) VALUES (:id, :email, :password, :ip_address)`
+	user.HashedPassword()
+	_, err := u.db.NamedExec(userSQL, user)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (u *UserService) List(first *int32, after *string) ([]*model.User, error) {
+	users := make([]*model.User, 0)
+	var fetchSize int32
+	if first == nil {
+		fetchSize = defaultListFetchSize
+	} else {
+		fetchSize = *first
+	}
+
+	if after != nil {
+		userSQL := `SELECT * FROM users WHERE created_at < (SELECT created_at FROM users WHERE id = $1) ORDER BY created_at DESC LIMIT $2;`
+		decodedIndex, _ := DecodeCursor(after)
+		err := u.db.Select(&users, userSQL, decodedIndex, fetchSize)
+		if err != nil {
+			return nil, err
+		}
+		return users, nil
+	}
+	userSQL := `SELECT * FROM users ORDER BY created_at DESC LIMIT $1;`
+	err := u.db.Select(&users, userSQL, fetchSize)
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (u *UserService) Count() (int, error) {
+	var count int
+	userSQL := `SELECT count(*) FROM users`
+	err := u.db.Get(&count, userSQL)
+	if err != nil {
+		return 0, err
+	}
+	return count, nil
+}
+
+func (u *UserService) ComparePassword(userCredentials *model.UserCredentials) (*model.User, error) {
+	user, err := u.FindByEmail(userCredentials.Email)
+	if err != nil {
+		return nil, errors.New(context.UnauthorizedAccess)
+	}
+	if result := user.ComparePassword(userCredentials.Password); !result {
+		return nil, errors.New(context.UnauthorizedAccess)
+	}
+	return user, nil
+}
